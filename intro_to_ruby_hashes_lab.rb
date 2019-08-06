91f5a502fb819c0635afbfedcea5f5b1a57f9617
Ryan McCahon <mccahonr@yahoo.com>
2019-08-06 22:12:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
   # complexity.
 	monopoly = {:railroads =>
 		{
-			:pieces => 4, 
+			:pieces => 4,
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
